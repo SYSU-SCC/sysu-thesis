ef74164960eafd71fcd27795d44315d12ad99575
1FCENdoge <438033873a@gmail.com>
2025-05-04 11:14:58 +0800

    合并本科生与研究生论文模板，并做到一键切换

--- fonts/init_fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeSongStd-Light.otf
-wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeHeitiStd-Regular.otf
-wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeKaitiStd-Regular.otf
-wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/11/raw/Resources/Fonts/AdobeFangsongStd-Regular.otf
